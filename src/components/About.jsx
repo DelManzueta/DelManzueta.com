5510b0561e139237e8d47e530476db3563087e64
DelManzueta <dmanzueta@gmail.com>
2021-05-27 21:34:49 -0400

    begain building about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,15 +3,28 @@ import '../styles/about.css'
 
 export default function About () {
   return (
-    <div className='angry-grid'>
-      <div id='item-0'>
-        <h2>Allow me to reintroduce myself</h2>
-        <h1>My Name is Del</h1>
-      </div>
-      <div id='item-1'>&nbsp;</div>
-      <div id='item-2'>&nbsp;</div>
-      <div id='item-3'>&nbsp;</div>
-      <div id='item-4'>&nbsp;</div>
+    <div className='about-container'>
+        <span>
+          Hi, I’m Del. After a long career as a visual designer, with web design
+          experience, I decided to make the full transition into web
+          development. My ability to translate client needs into successful
+          user-centered design for print and digital products opened the door to
+          web design, which lead to working directly with front-end technologies
+          such as HTML, CSS and a bit of JavaScript.
+        </span>
+        <span>
+          My interest in web development grew when I realized my natural
+          creative talent was perfectly complimented with my passion for problem
+          solving, allowing me to translate complex ideas into innovative
+          solutions. I offer a comprehensive understanding of human
+          decision-making behaviors and demonstrate the ability to work
+          intuitively within cross-functional teams.
+        
+          Aside from my technical abilities, my strengths lie in anticipating
+          uncommon issues regarding usability, and production, as well as
+          performing at the highest standards, regardless of capacity and
+          limitations.
+        </span>
     </div>
   )
 }
