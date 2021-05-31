24e2ce8577051be6ea907a975e9f562195105579
DelManzueta <dmanzueta@gmail.com>
2021-05-31 02:49:51 -0400

    added about cards skeleton (styling is missing)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,47 +6,38 @@ import AboutCards from './AboutCards'
 export class About extends React.Component {
   render () {
     return (
-      <div>
-        <section className='about-container'>
-          <div className='row'>
-            <div className='col grid_1_of_3'>
-              <img className='del-img' src={selfie} alt='' />
-            </div>
-            <div className='col grid_2_of_3'>
-              <div className='about-main'>
-                <h1>Allow me to reintroduce myself,</h1>
-                <div className='col grid_2_of_2'>
-                  <p className='about-txt'>
-                    My name is Del - and after a long career as a visual
-                    designer, specializing in UX/UI &amp; Front-End Design, I've
-                    decided to make the full transition into web development,
-                    with a direct focus on the MERN stack. My ability to
-                    translate client needs into successful user-centered design
-                    for print and digital products opened the door to web
-                    design, which lead to working directly with front-end
-                    technologies such as HTML, CSS and a bit of JavaScript.
-                  </p>
-                </div>
-                <div className='col grid_2_of_2'>
-                  <p className='about-txt'>
-                    My interest in web development grew when I realized my
-                    natural creative talent was perfectly complimented with my
-                    passion for problem solving, allowing me to translate
-                    complex ideas into innovative solutions. Aside from my
-                    technical abilities, my strengths lie in anticipating
-                    uncommon issues regarding usability, and production, as well
-                    as performing at the highest standards, regardless of
-                    capacity and limitations.
-                  </p>
-                </div>
-              </div>
-            </div>
+      <div className='about-container'>
+
+        <section className='about-hero'>
+          <img src={selfie} alt='Delwing Manzueta' />
+          <div className='about-copy'>
+            <h1>Allow me to reintroduce myself, </h1>
+            <p>
+              My name is Del - and after a long career as a visual designer,
+              specializing in UX/UI &amp; Front-End Design, I've decided to make
+              the full transition into web development, with a direct focus on
+              the MERN stack. My ability to translate client needs into
+              successful user-centered design for print and digital products
+              opened the door to web design, which lead to working directly with
+              front-end technologies such as HTML, CSS and a bit of JavaScript.
+            </p>
+            <p>
+              My interest in web development grew when I realized my natural
+              creative talent was perfectly complimented with my passion for
+              problem solving, allowing me to translate complex ideas into
+              innovative solutions. Aside from my technical abilities, my
+              strengths lie in anticipating uncommon issues regarding usability,
+              and production, as well as performing at the highest standards,
+              regardless of capacity and limitations.
+            </p>
           </div>
         </section>
- 
-        <section>
-         <AboutCards/>
-        </section> 
+
+        <section className='divider' />
+
+        <section className='about-cards'>
+          <AboutCards />
+        </section>
       </div>
     )
   }
--- src/components/AboutCards.jsx
+++ src/components/AboutCards.jsx
@@ -1,60 +1,62 @@
 import React from 'react'
 import '../styles/ac.css'
-import Dev from '../img/dev.png'
-import Spray from '../img/spray.png'
-import Graph from '../img/design.png'
 
 const AboutCards = () => {
   return (
-    <div class='ac-container'>
-      <div class='card'>
-        <div class='face face1'>
-          <div class='content'>
-            <div class='icon'>
-              <img src={Dev} alt="" />
+    <div className='ac-container'>
+      <section className='ac-section ac-section-padded'>
+        <div className='section-container'>
+          <div className='ac-row title'>
+            <h2>Services</h2>
+            <h4 className='light muted'>
+              Achieve the best results with our wide variety of training
+              options!
+            </h4>
+          </div>
+          <div className='ac-row services'>
+            <div className='col-md-4'>
+              <div className='service'>
+                <div className='icon-holder'>
+                  <img src='img/icons/heart-blue.png' alt='' className='icon' />
+                </div>
+                <h4 className='heading'>Front-End Dev</h4>
+                <p className='description'>
+                  A elementum ligula lacus ac quam ultrices a scelerisque
+                  praesent vel suspendisse scelerisque a aenean hac montes.
+                </p>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='service'>
+                <div className='icon-holder'>
+                  <img src='img/icons/guru-blue.png' alt='' className='icon' />
+                </div>
+                <h4 className='heading'>Visual Design</h4>
+                <p className='description'>
+                  A elementum ligula lacus ac quam ultrices a scelerisque
+                  praesent vel suspendisse scelerisque a aenean hac montes.
+                </p>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='service'>
+                <div className='icon-holder'>
+                  <img
+                    src='img/icons/weight-blue.png'
+                    alt=''
+                    className='icon'
+                  />
+                </div>
+                <h4 className='heading'>Critical Thinking</h4>
+                <p className='description'>
+                  A elementum ligula lacus ac quam ultrices a scelerisque
+                  praesent vel suspendisse scelerisque a aenean hac montes.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div class='face face2'>
-          <div class='content'>
-            
-            <p>This is where I network and build my professional portfolio.</p>
-          </div>
-        </div>
-      </div>
-      <div class='card'>
-        <div class='face face1'>
-          <div class='content'>
-            <div class='icon'>
-              <img src={Graph} alt="" />
-            </div>
-          </div>
-        </div>
-        <div class='face face2'>
-          <div class='content'>
-            
-            <p>
-              This is where I read news and network with different social
-              groups.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class='card'>
-        <div class='face face1'>
-          <div class='content'>
-            <div class='icon'>
-              <img src={Spray} alt="" />
-            </div>
-          </div>
-        </div>
-        <div class='face face2'>
-          <div class='content'> 
-            <p>This is where I share code and work on projects.</p>
-          </div>
-        </div>
-        
-      </div> 
+      </section>
     </div>
   )
 }
